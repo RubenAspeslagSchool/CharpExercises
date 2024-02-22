8838b6cb3d55b8015d388c45ed9b43211d66906b
R. Aspeslag <ruben.aspeslag@student.howest.be>
2024-02-22 14:36:32 +0100

    add advancedTopics exercise 1 yield fibonacii

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // 1A
+            Fibonacci(10)
+                .ToList()
+                .ForEach(number => Console.WriteLine(number));
+        }
+
+        static IEnumerable<int> Fibonacci(int count)
+        {
+            int a = 0,
+            b = 1;
+
+            for (int i = 0; i < count; i++)
+            {
+                yield return a;
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
         }
     }
 }
