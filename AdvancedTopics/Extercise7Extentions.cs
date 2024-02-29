5dc3ffc3ad9ac40a7868cb3554626babe5da2ea6
R. Aspeslag <ruben.aspeslag@student.howest.be>
2024-02-29 17:16:27 +0100

    Advanced topics exercise 7

---
+++ AdvancedTopics/Extercise7Extentions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTopics
+{
+    public static class Extercise7Extentions
+    {
+        public static IEnumerable<T> WhereEx7<T>(this IEnumerable<T> list, Func<T,bool> filter)
+        {
+            foreach (T item in list)
+            {
+                if(filter(item)) yield return item;
+            }
+        }
+
+        public static List<T> ToListEx7<T>(this IEnumerable<T> list)
+        {
+            List<T> result = new List<T>();
+            foreach(T item in list)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+
+        public static void ForEachEx7<T>( this List<T>list ,Action<T> f )
+        {
+            foreach(T item in list) { f(item); }
+        }
+
+    }
+}
---
+++ UnitTest/AdvancedTopic/Exercise7.cs
@@ -0,0 +1,27 @@
+﻿using AdvancedTopics;
+using System;
+using System.Collections.Generic;
+// using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.AdvancedTopic
+{
+    public class Exercise7
+    {
+
+        [Fact]
+        public void Result_Extentions_ReturnIEnumerable()
+        {
+            //Arrange
+            IEnumerable<int> numbers = new List<int>() { 5, 67, 23, 8, 45 };
+            // Act
+            numbers
+            .WhereEx7(n => n > 15) //your own extension method
+            .ToListEx7() //your own extension method
+            .ForEachEx7(n => Console.WriteLine(n)); //your own extension method
+            // Assert
+        }
+
+    }
+}
