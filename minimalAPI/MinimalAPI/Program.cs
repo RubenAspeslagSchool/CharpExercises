d9082b3d298195082870540ba76828be11ca464f
R. Aspeslag <ruben.aspeslag@student.howest.be>
2024-03-02 01:17:43 +0100

    minimal API exercise 3

--- minimalAPI/MinimalAPI/Program.cs
+++ minimalAPI/MinimalAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MinimalAPI.Routes;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
@@ -18,47 +19,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-StudentRepository studentRepository = new StudentRepository();
+
 
 RouteGroupBuilder studentsGroup = app.MapGroup("/students")
                 .WithTags("Students");
 
-studentsGroup.MapGet("/", () =>
-{
-    return Results.Ok(studentRepository.GetAllStudents());
-});
-
-
-studentsGroup.MapGet("/{id:int}", (int id) =>
-{
-    Student student = studentRepository.GetStudentById(id);
-    return student != null
-        ? Results.Ok(student)
-        : Results.NotFound($"student with ID {id} not fount");
-});
-
-studentsGroup.MapPost("/", (string name, bool succeded) =>
-{
-     return Results.Ok(studentRepository.AddStudent(name, succeded));
-});
-
-
-studentsGroup.MapPut("/{id:int}", (int id, string name, bool succeded) =>
-{
-    studentRepository.PutStudent(id, name, succeded);
-    return Results.Ok(studentRepository.GetAllStudents());
-});
-
-studentsGroup.MapDelete("/{id:int}", (int id) =>
-{
-    studentRepository.DeleteStudent(id);
-    return Results.Ok(studentRepository.GetAllStudents());
-});
-
-studentsGroup.MapGet("/random", ([FromQuery] int random) => 
-{
-    return Results.Ok(studentRepository.GetRandom(random));
-});
+StudentRoutes studentRoutes = new StudentRoutes();
+studentRoutes.AddRoutes(studentsGroup);
 
 app.Run();
 
---
+++ minimalAPI/MinimalAPI/Routes/StudentRoutes.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalAPI.Routes
+{
+    public class StudentRoutes
+    {
+        public StudentRepository studentRepository { get; init; }
+        public StudentRoutes()
+        {
+            studentRepository = new StudentRepository();
+        }
+        public void AddRoutes(RouteGroupBuilder studentsGroup)
+        {
+
+
+            studentsGroup.MapGet("/", () =>
+            {
+                return Results.Ok(studentRepository.GetAllStudents());
+            });
+
+
+            studentsGroup.MapGet("/{id:int}", (int id) =>
+            {
+                Student student = studentRepository.GetStudentById(id);
+                return student != null
+                    ? Results.Ok(student)
+                    : Results.NotFound($"student with ID {id} not fount");
+            });
+
+            studentsGroup.MapPost("/", (string name, bool succeded) =>
+            {
+                return Results.Ok(studentRepository.AddStudent(name, succeded));
+            });
+
+
+            studentsGroup.MapPut("/{id:int}", (int id, string name, bool succeded) =>
+            {
+                studentRepository.PutStudent(id, name, succeded);
+                return Results.Ok(studentRepository.GetAllStudents());
+            });
+
+            studentsGroup.MapDelete("/{id:int}", (int id) =>
+            {
+                studentRepository.DeleteStudent(id);
+                return Results.Ok(studentRepository.GetAllStudents());
+            });
+
+            studentsGroup.MapGet("/random", ([FromQuery] int random) =>
+            {
+                return Results.Ok(studentRepository.GetRandom(random));
+            });
+
+        }
+    }
+}
