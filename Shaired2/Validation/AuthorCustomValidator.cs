0ab3b8cc82d484e271287a28bb68604ff5c4cd0f
R. Aspeslag <ruben.aspeslag@student.howest.be>
2024-03-21 17:10:25 +0100

    m

---
+++ Shaired2/Validation/AuthorCustomValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace Shared.Validation
+{
+    public class AuthorCustomValidator: AbstractValidator<PublisherWriteDTO>
+    {
+        public AuthorCustomValidator()
+        {
+            RuleFor(a => a.Name)
+                .NotEmpty()
+                .WithMessage("The first name of the author cannot be blank")
+                .Length(2, 50)
+                .WithMessage("The first name should be at least 2 characters and cannot be more than 50 characters.");
+
+            RuleFor(a => a.Lastname)
+                .NotEmpty()
+                .WithMessage("The last name of the author cannot be blank")
+                .Length(2, 50)
+                .WithMessage("The last Name should be at least 2 characters and cannot be more than 50 characters.");
+
+            RuleFor(a => a.City)
+                .Length(0, 80)
+                .WithMessage("The place to live cannot be more than 80 characters.")
+                .Custom((c, context) => {
+                    IEnumerable<string> cities = new List<string> { "Brugge", "Gent", "Leuven" };
+                    if (!cities.Contains(c))
+                    {
+                        context.AddFailure("Wrong city!!");
+                    }
+                });
+        }
+    }
+}
